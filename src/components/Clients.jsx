5e833d98734a4016e5420c991f57ac76f29167d6
Diego Vilhlava <diegorocha450@yahoo.com>
2024-04-12 13:48:04 -0300

    footer component finished

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,8 +1,18 @@
+import {clients} from "../constants"
+import styles from "../style"
 
 
 const Clients = () => {
   return (
-    <div>Clients</div>
+    <section className={`${styles.flexCenter} my-4`}>
+        <div className={`${styles.flexCenter} flex-wrap w-full`}>
+            {clients.map((client) => (
+              <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px]`}>
+                    <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain client-logo"  />
+              </div>
+            ))}
+        </div>
+    </section>
   )
 }
 
