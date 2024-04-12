587e31792732d6625510d23bdf0019d590bd17e8
Diego Vilhlava <diegorocha450@yahoo.com>
2024-04-12 11:22:27 -0300

    testimonials component finished

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,8 +1,27 @@
+import { card } from "../assets"
 
+import styles, { layout } from "../style"
+
+import Button from "./Button"
 
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better card deal <br className="sm:block hidden" /> in few easy
+          steps.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+
+          A bank card deal offers perks like cashback or travel rewards, with terms on fees and interest rates. It provides convenience and financial benefits for users.
+        </p>
+        <Button styles="mt-10"/>
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className="w-[100%] h-[100%]" />
+      </div>
+    </section>
   )
 }
 
